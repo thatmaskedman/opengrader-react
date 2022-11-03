863e08d88d95de4f1b60b93b0e895e28214aad80
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-11-02 23:16:45 -0700

    adding proxy environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import * as dotenv from 'dotenv'
 import react from '@vitejs/plugin-react'
 
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.API_ROOT,
+        changeOrigin: true,
+        secure: false,      
+        ws: true,
+      }
+    }
+  }
 })
