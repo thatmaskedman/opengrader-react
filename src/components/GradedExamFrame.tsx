b75ebfc371bcac56e10f66f48e3fd9ed9e639adf
Jordan Ibarra Ashurst <bxordan@gmail.com>
2023-09-18 01:48:53 -0700

    adding react query to frame

--- src/components/GradedExamFrame.tsx
+++ src/components/GradedExamFrame.tsx
@@ -1,18 +1,47 @@
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import { useEffect } from "react"
+import { ExamData } from "../global/types"
+import Spinner from "./ui/Spinner"
 
 interface GradedExamFrameProps {
-    url: string | undefined
+    url: string | undefined 
+    examID: number
 }
 
-const GradedExamFrame = (props: GradedExamFrameProps) => {
+const GradedExamFrame = (props: GradedExamFrameProps) => {    
+    const fetchExam = (): Promise<ExamData> => 
+        axios.get(`/api/exams/${props.examID}` )
+            .then(res => res.data)
+
+    const examQuery = useQuery({
+        queryKey: ['examImages', props.examID],
+        queryFn: fetchExam
+    })
+
+    const {data, isLoading, isError} = examQuery
     
+    if (isLoading) {
+        return <></>
+    }
+    
+    if (isError) {
+        return <></>
+    }
+        
+
     return (
             <>
-            <div className='bg-gray-100 p-4 m-5'>
-                <img src={props.url} className='object-contain w-96 h-96' />
-            </div>
+                <div className='bg-gray-100 p-4 m-5'>
+                    <img src={data.exam_image_original} key={data.exam_image_original} className='object-contain w-96 h-96' />
+                </div>
+                <div className='bg-gray-100 p-4 m-5'>
+                    <img src={data.exam_image_grid} key={data.exam_image_grid} className='object-contain w-96 h-96' />
+                </div>
+                <div className='bg-gray-100 p-4 m-5'>
+                    <img src={data.exam_image_graded} key={data.exam_image_graded} className='object-contain w-96 h-96' />
+                </div>
+
             </>
     )
 }
