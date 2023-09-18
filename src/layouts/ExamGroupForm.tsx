2ba7454cd8e06b1d370368f9895b79108f981d79
Jordan Ibarra Ashurst <bxordan@gmail.com>
2023-09-18 01:45:46 -0700

    adding max and min to attribute

--- src/layouts/ExamGroupForm.tsx
+++ src/layouts/ExamGroupForm.tsx
@@ -78,6 +78,8 @@ const ExamGroupForm = () => {
                     name="numQuestions"
                     id="numQuestions"
                     placeholder="Questions number"
+                    min="1" 
+                    max="50"
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                     />
                 </div>
